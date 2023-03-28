9d67fb407936f8af6a0542df0ccfb599a98c66bf
DESKTOP-K308J3C\Karim <karimgt@gmail.com>
2023-03-28 04:25:37 -0600

    ADD: Fast api endpoint with input input schemas.

---
+++ end_point/schemas/features.py
@@ -0,0 +1,60 @@
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+#class Operador(str, Enum):
+_operator_list = ('Grupo LATAM',  'Sky Airline', 'Aerolineas Argentinas', 'Copa Air',
+    'Latin American Wings', 'Avianca', 'JetSmart SPA', 'Gol Trans',
+    'American Airlines', 'Air Canada', 'Iberia', 'Delta Air', 'Air France',
+    'Aeromexico', 'United Airlines', 'Oceanair Linhas Aereas', 'Alitalia',
+    'K.L.M.', 'British Airways', 'Qantas Airways', 'Lacsa', 'Austral',
+    'Plus Ultra Lineas Aereas')
+
+operators = tuple(x.lower() for x in _operator_list)
+
+
+def operator_index(index: int):
+    try:
+        return operators[index]
+
+    except IndexError:
+        return None
+
+
+
+class TipoVuelo(str, Enum):
+    nacional: 'N'
+    internacional: 'I'
+
+
+class Mes(int, Enum):
+    enero: 0
+    febrero: 1
+    marzo: 2
+    abril: 3
+    mayo: 4
+    junio: 5
+    julio: 6
+    agosto: 7
+    septiembre: 8
+    octubre: 9
+    noviembre: 10
+    diciembre: 11
+
+
+class PredictionInput(BaseModel):
+    operator: str  # o poner int?
+    flight_type: TipoVuelo
+    month: Mes
+
+
+class PredictionOutput(BaseModel):
+    delay_proba = str
+
+
+
+delay_labels = {
+    0: 'No Delay',
+    1: 'Delay'
+}
