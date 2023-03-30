b771448261ab7b7998abcfc418380e1f3350005f
DESKTOP-K308J3C\Karim <karimgt@gmail.com>
2023-03-30 15:19:51 -0600

    test update

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,12 +7,12 @@ def test_plain_inference():
     model = airport_delay_model()
     input_shape = model.feats_in
     randnums = np.random.randint(0, 2, size=input_shape)
-    r = model(np.reshape(randnums, (1, -1)))
-    assert r
+    inference = model(np.reshape(randnums, (1, -1)))
+    assert len(inference) > 0
 
 
 def test_model_predict():
     sample = ['sky airline', 'I', '8']
     model = airport_delay_model()
     r = model.predict(sample)
-    assert r
+    assert len(r) > 0
